bd79b74b55e1238f1de2577143a03af01f07d524
NucyLoodle <lucymeredith3@gmail.com>
2024-08-07 10:12:53 +0100

    add pokemon and pokemon_moves tables

--- sql-files/pokemon-game.sql
+++ sql-files/pokemon-game.sql
@@ -15,3 +15,26 @@ FOREIGN KEY (user_id)
   REFERENCES user_profile(user_id)
   ON DELETE CASCADE
   );
+
+CREATE TABLE pokemon (
+user_id INT NOT NULL UNIQUE,
+pokemon_id INT PRIMARY KEY,
+pokemon_name VARCHAR(20) NOT NULL,
+pokemon_type VARCHAR(20) NOT NULL,
+CONSTRAINT fk_user_id_pokemon
+FOREIGN KEY (user_id)
+  REFERENCES user_profile(user_id)
+  ON DELETE CASCADE
+);
+
+CREATE TABLE pokemon_moves (
+pokemon_id INT NOT NULL UNIQUE,
+move_1 VARCHAR(15),
+move_2 VARCHAR(15),
+move_3 VARCHAR(15),
+move_4 VARCHAR(15),  
+CONSTRAINT fk_user_id_pokemon_moves
+FOREIGN KEY (pokemon_id)
+  REFERENCES pokemon(pokemon_id)
+  ON DELETE CASCADE
+);
