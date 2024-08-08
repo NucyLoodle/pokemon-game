1601aee6829f4b1e6865a93da581b4419143bd17
NucyLoodle <lucymeredith3@gmail.com>
2024-08-08 11:44:38 +0100

    fix links to static files

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 
-app = Flask(__name__ , template_folder='../frontend/templates')
+app = Flask(__name__ ,
+            static_url_path='',
+            static_folder='../frontend/static',
+            template_folder='../frontend/templates')
 
 @app.route("/")
 def main():
