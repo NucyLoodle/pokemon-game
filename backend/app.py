84a55e1f470b1bec3cfeb06d6bf86c5d6dcea2a2
NucyLoodle <lucymeredith3@gmail.com>
2024-08-08 10:38:41 +0100

    define basic route and request handler

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__ , template_folder='../frontend/templates')
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
