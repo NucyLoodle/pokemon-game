7e79ff6ebb98670e6534e4febc3d67763c86f4b8
NucyLoodle <lucymeredith3@gmail.com>
2024-08-09 10:29:54 +0100

    remove comments

--- backend/battle.py
+++ backend/battle.py
@@ -11,56 +11,26 @@ Functions to create the initial battle between player and CPU
 # get the three starter pokemon from the pokeAPI
 """
 later: display images of pokemon
-image = data['sprites']['front_default']
-
-
-id = "1"
-api_url = "https://pokeapi.co/api/v2/pokemon/{0}".format(id)
-response = requests.get(url=api_url)
-data = response.json()
-
-
-name = data['name']
-
 later: display pokemon stats, type, and randomise available moves"""
 
 # ask player to choose their pokemon
 pokemon_name = input("Now, which Pokemon do you want? The three available Pokemon are Bulbasaur, Charmander, and Squirtle.").lower()
-# up to four moves are allocated to the player's pokemon randomly
-# filter data by level-learned-at: 1 and mover_learn_method: name: "level-up"
 
+# get response from API
 api_url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
 response = requests.get(api_url)
 data = response.json()
 
 def get_initial_moves():
+    # filter data by level-learned-at: 1 and mover_learn_method: name: "level-up"
     move_names = []
     for i in range(len(data)):
         if data['moves'][i]['version_group_details'][0]['move_learn_method']['name'] == "level-up" and data['moves'][i]['version_group_details'][0]['level_learned_at'] == 1:
             move_names.append(data['moves'][i]['move']['name'])
     return move_names
             
-
-print(get_initial_moves())
-
-
-
-#print(data[1]['version_group_details'][0]['move_learn_method']['name']) 
-
-#pp(data[104]['move']['name']) 
-"""
-useful queries
-
-pp(data[0]['move']['name']) 
-#gets the name of the first move
-
-pp(data[0]['version_group_details'][0]['level_learned_at'])
-# gets the level the first move is learned at
-
-pp(data[0]['version_group_details'][1]['move_learn_method']['name'])
-# gets the way the move is learned
-
-"""
+# up to four moves are allocated to the player's pokemon randomly
+print(f"The moves that {pokemon_name} has are {get_initial_moves()}")
 
 # store this pokemon in the db
 # cpu randomly chooses pokemon from remaining two. 
