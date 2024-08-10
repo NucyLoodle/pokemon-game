63dd543271c9a8a89d7e0a719bed9909dbd528b3
NucyLoodle <lucymeredith3@gmail.com>
2024-08-10 19:40:43 +0100

    write functions to send data to backend

--- frontend/static/js/battle.js
+++ frontend/static/js/battle.js
@@ -1,8 +1,40 @@
-// select buttons
+function handleResponse(response) {
+    return;
+    }
 
 const buttons = document.querySelector(".pokemon-choice-buttons");
-console.log(buttons)
+let pokemonDict = {};
+function choosePokemon() {
+    buttons.addEventListener('click', event => {
+        const {target} = event; //extracts target from the event info
+        const {value} = target; //extract the value property from the target ie the number clicked!
+        pokemonDict["name"] = value
+        console.log(pokemonDict)
+        if (!target.matches('button')) { //check the user clicked a button
+        
+            return;
+        } else {
+            let apiUrl = "http://127.0.0.1:5001/battle/";
+            axios
+                .get(apiUrl, pokemonDict)
+                .then(handleResponse)
+                .catch((err) => console.log(err));
+        }
+        }
+    )}
+    
+    
 
-buttons.addEventListener('click', event => {
-    console.log("clicked")
-})
+
+choosePokemon()
+
+// select buttons
+
+
+
+// buttons.addEventListener('click', event => {
+//     console.log("clicked")
+//     const {target} = event; //extracts target from the event info
+//     const {value} = target; //extract the value property from the target ie the number clicked!
+//     choosePokemon(value)
+// })
