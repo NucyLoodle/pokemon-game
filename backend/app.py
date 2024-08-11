33f341ddce11f9a0742bf5c34cb0368e8098fde0
NucyLoodle <lucymeredith3@gmail.com>
2024-08-11 14:15:11 +0100

    edit print statement

--- backend/app.py
+++ backend/app.py
@@ -17,11 +17,12 @@ def battle_page():
 @app.route("/battle/", methods=['GET', 'POST'])
 def get_user_choice():
     user_pokemon_name = request.form['userPokemonChoice']
+    print(f"the user has chosen {user_pokemon_name}")
+
     cpu_pokemon_name = battle.cpu_pokemon_choice(user_pokemon_name)
     user_pokemon = dict(name = f"{user_pokemon_name}", hp = battle.get_hp_stat(user_pokemon_name), moves = battle.get_initial_moves(user_pokemon_name) )
     cpu_pokemon = dict(name = f"{cpu_pokemon_name}", hp = battle.get_hp_stat(cpu_pokemon_name), moves = battle.get_initial_moves(cpu_pokemon_name) )
-    #print(user_pokemon_name)
-
+    
     return battle.battle(cpu_pokemon, user_pokemon)
 
 
