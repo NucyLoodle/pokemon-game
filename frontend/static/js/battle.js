eeaf40ae18fdf1ebcd284f22886cc77eab6fe8cc
NucyLoodle <lucymeredith3@gmail.com>
2024-08-11 20:51:19 +0100

    hide and reveal elements - currently not working

--- frontend/static/js/battle.js
+++ frontend/static/js/battle.js
@@ -1,10 +1,30 @@
-const choiceForm = document.getElementById("choosePokemon")
+const choiceForm = document.getElementById("userPokemonChoice")
 console.log(choiceForm)
+const choiceButtons = document.querySelectorAll("#choosePokemon")
+console.log(choiceButtons)
 const launchGame = document.getElementById("launchGame")
+launchGame.style.display = "none"
 
 
-launchGame.addEventListener("click", function() {
-        choiceForm.style.display = "none"
-        
+choiceButtons.forEach(function(button){
+    button.addEventListener("click", function() {
+        //choiceForm.style.display = "none"
+        launchGame.style.display = "block"
     })
+})
 
+
+
+
+
+
+
+
+
+// launchGame.addEventListener("click", function() {
+//         //choiceForm.style.display = "none"
+//         choiceForm.remove()
+//         launchGame.style.display = "block"
+
+        
+//     })
