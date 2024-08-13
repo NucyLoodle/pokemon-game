b0d6de7b6ba5585404598c5f5a86b89842f8b5af
NucyLoodle <lucymeredith3@gmail.com>
2024-08-13 08:19:57 +0100

    rewrite get_user_choice for dynamic loading

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, after_this_request
+from flask_cors import CORS
 import battle
 
 app = Flask(__name__ ,
@@ -6,29 +7,43 @@ app = Flask(__name__ ,
             static_folder='../frontend/static',
             template_folder='../frontend/templates')
 
+# CORS(app)
+
 @app.route("/")
 def main():
     return render_template('index.html')
     
-@app.route("/battle/")
+@app.route("/battle")
 def battle_page():
     return render_template('battle.html')
 
-@app.route("/battle/", methods=['GET', 'POST'])
+@app.route("/battle", methods=['POST'])
 def get_user_choice():
     user_pokemon_name = request.form['userPokemonChoice']
-    #print(f"the user has chosen {user_pokemon_name}")
-
-    cpu_pokemon_name = battle.cpu_pokemon_choice(user_pokemon_name)
-    user_pokemon = dict(name = f"{user_pokemon_name}", hp = battle.get_hp_stat(user_pokemon_name), moves = battle.get_initial_moves(user_pokemon_name) )
-    cpu_pokemon = dict(name = f"{cpu_pokemon_name}", hp = battle.get_hp_stat(cpu_pokemon_name), moves = battle.get_initial_moves(cpu_pokemon_name) )
-    
+    print(user_pokemon_name)
     user_choice = f"You have chosen {user_pokemon_name.capitalize()}."
-    pokemon_hp = f"{user_pokemon_name.capitalize()}'s hp is {user_pokemon['hp']}"
-    pokemon_moves = f"{user_pokemon_name.capitalize()}'s moves are {', '.join(user_pokemon['moves'])}"
+    return user_choice
+   
 
-    #return battle.battle(cpu_pokemon, user_pokemon)
-    return render_template('battle.html', userChoice=user_choice, pokemonHp=pokemon_hp, pokemonMoves=pokemon_moves)
+    # user_pokemon_name = request.form['userPokemonChoice']
+    # user_choice = f"You have chosen {user_pokemon_name.capitalize()}."
+    # return user_choice
+
+
+# def get_user_choice():
+#     user_pokemon_name = request.form['userPokemonChoice']
+#     #print(f"the user has chosen {user_pokemon_name}")
+
+#     cpu_pokemon_name = battle.cpu_pokemon_choice(user_pokemon_name)
+#     user_pokemon = dict(name = f"{user_pokemon_name}", hp = battle.get_hp_stat(user_pokemon_name), moves = battle.get_initial_moves(user_pokemon_name) )
+#     cpu_pokemon = dict(name = f"{cpu_pokemon_name}", hp = battle.get_hp_stat(cpu_pokemon_name), moves = battle.get_initial_moves(cpu_pokemon_name) )
+    
+#     user_choice = f"You have chosen {user_pokemon_name.capitalize()}."
+#     pokemon_hp = f"{user_pokemon_name.capitalize()}'s hp is {user_pokemon['hp']}"
+#     pokemon_moves = f"{user_pokemon_name.capitalize()}'s moves are {', '.join(user_pokemon['moves'])}"
+
+#     #return battle.battle(cpu_pokemon, user_pokemon)
+#     return render_template('battle.html', userChoice=user_choice, pokemonHp=pokemon_hp, pokemonMoves=pokemon_moves)
 
 #def take_turns():
 
