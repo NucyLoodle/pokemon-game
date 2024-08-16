0a6cabe69400036d80bef380419e99ead970e998
NucyLoodle <lucymeredith3@gmail.com>
2024-08-16 11:08:24 +0100

    attach event listener

--- frontend/static/js/battle.js
+++ frontend/static/js/battle.js
@@ -60,7 +60,8 @@ launchGameForm.addEventListener('submit', function(e) {
 
             new_form = gamePlay.appendChild(document.createElement("form"))
             new_form.setAttribute("method", "POST")
-            new_form.setAttribute("action", "/battle/turn")
+            new_form.setAttribute("action", "/battle/launch/turn")
+            new_form.setAttribute("id", "battleForm")
             new_button_1 = new_form.appendChild(document.createElement("button"))
             new_button_1.setAttribute("type", "submit")
             new_button_1.setAttribute("name", "chooseMove")
@@ -71,10 +72,45 @@ launchGameForm.addEventListener('submit', function(e) {
             new_button_2.setAttribute("type", "submit")
             new_button_2.setAttribute("name", "chooseMove")
             new_button_2.setAttribute("value", `${data[0]['moves'][1]}`)
-            new_button_2.textContent =  `${data[0]['moves'][1]}`
+            new_button_2.textContent = `${data[0]['moves'][1]}`
 
-
+            
         });
+})
+
+new_form = document.getElementById("battleForm")
+console.log(new_form)
+new_form.addEventListener('submit', function(e) {
+
+    e.preventDefault();
+    console.log("hey")
+    
+
+    // // Create a form with user input.
+    // const userFormData = new FormData(this); 
+    // // Add the name and value of the pressed button to the form.
+    // userFormData.append(e.submitter.name, e.submitter.value); 
+    // console.log(e.submitter.name)
+    // console.log(e.submitter.value)
+    // // Send a fetch request of type POST.
+    // const url = '/battle/launch';
+    // fetch(url, { 
+    //     method: 'post', 
+    //     body: userFormData
+    // })
+    //     .then(response => response.json()) // Read the response as json.
+    //     .then(data => {
+    //         console.log(data)
+    //         // console.log(data[0]['moves'])
+
+    //         // new_para = new_form.appendChild(document.createElement("p"))
+
+    //         // new_para.innerText = `${data[0]['name']} used ${data[0]['move']} causing ${data[0]['damage']} damage.
+    //         // ${data[1]['name']}'s hp was reduced to ${data[1]['hp']}.
+            
+
+
+    //     });
 })
 
 
@@ -93,4 +129,3 @@ launchGameForm.addEventListener('submit', function(e) {
 //         //choiceSection.style.display = "none"
 //         launchGame.style.display = "block"
 //     })
-// })
