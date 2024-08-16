5e86ac6850e9b909f7d49294144c53625ecfd0fe
NucyLoodle <lucymeredith3@gmail.com>
2024-08-16 21:26:17 +0100

    refactor code

--- frontend/static/js/battle.js
+++ frontend/static/js/battle.js
@@ -5,35 +5,50 @@ const launchGameForm = document.getElementById("launchGameForm")
 const gamePlay = document.getElementById("gamePlay")
 launchGame.style.display = "none"
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
 
-    // Create a form with user input.
-    const formData = new FormData(this); 
-    // Add the name and value of the pressed button to the form.
-    formData.append(e.submitter.name, e.submitter.value); 
-    // Send a fetch request of type POST.
-    const url = '/battle';
-    fetch(url, { 
-        method: 'post', 
-        body: formData
-    })
-        .then(response => response.json()) // Read the response as json.
-        .then(data => {
-            console.log(data);
-            pokemonStats = document.getElementById('stats')
-            // user_pokemon = data[0]['name']
-            // pokemonName = data['name']
-            // pokemonMoves = data['moves'].join(', ')
-            pokemonStats.innerText = 
-                `You have chosen ${data[0]['name']}. ${data[0]['name']}'s hp is ${data[0]['hp']}. ${data[0]['name']}'s moves are ${data[0]['moves'].join(', ')}.
-                The cpu has chosen ${data[1]['name']}.`
-        });
+function getPokemonStats() {
 
-    choiceSection.style.display = "none"
-    launchGame.style.display = "block"
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+    
+        // Create a form with user input.
+        const formData = new FormData(this); 
+        // Add the name and value of the pressed button to the form.
+        formData.append(e.submitter.name, e.submitter.value); 
+        // Send a fetch request of type POST.
+    
+        // generate url
+        const url = '/battle';
+        fetch(url, { 
+            method: 'post', 
+            body: formData
+        })
+            .then(response => response.json()) // Read the response as json.
+            .then(data => {
+                displayPokemonStats(data)
+                
+            });
+    
+        choiceSection.style.display = "none"
+        launchGame.style.display = "block"
+    
+    
+    });
+    
 
-});
+}
+
+displayPokemonStats = function(data) {
+    pokemonStats = document.getElementById('stats')
+    pokemonStats.innerText = 
+        `You have chosen ${data[0]['name']}. ${data[0]['name']}'s hp is ${data[0]['hp']}. ${data[0]['name']}'s moves are ${data[0]['moves'].join(', ')}.
+        The cpu has chosen ${data[1]['name']}.`
+}
+
+
+
+
+
 
 launchGameForm.addEventListener('submit', function(e) {
     
@@ -109,4 +124,4 @@ launchGameForm.addEventListener('submit', function(e) {
         });
 })
 
-
+getPokemonStats()
