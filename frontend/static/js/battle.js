ceb3349a0c69457d88de937b18595f618849d5b8
NucyLoodle <lucymeredith3@gmail.com>
2024-08-17 10:17:17 +0100

    refactor code into functions

--- frontend/static/js/battle.js
+++ frontend/static/js/battle.js
@@ -47,168 +47,92 @@ displayPokemonStats = function(data) {
 
 
 function cpuTurn(){
+    launchGameForm.addEventListener('submit', function(e) {
 
-
+        launchGame.style.display = "none"
+        e.preventDefault();
+    
+        // Create a form with user input.
+        const cpuFormData = new FormData(this); 
+        // Add the name and value of the pressed button to the form.
+        cpuFormData.append(e.submitter.name, e.submitter.value); 
+        // Send a fetch request of type POST.
+        const url = '/battle/launch';
+        fetch(url, { 
+            method: 'post', 
+            body: cpuFormData
+        })
+            .then(response => response.json()) // Read the response as json.
+            .then(data => {
+            
+                //console.log(data)
+                //console.log(data[0]['moves'])
+    
+                new_para = launchGameForm.appendChild(document.createElement("p"))
+                new_para.innerText = `${data[1]['name']} used ${data[1]['move']} causing ${data[1]['damage']} damage.
+                ${data[0]['name']}'s hp was reduced to ${data[0]['hp']}.
+                Choose your next move!`
+    
+                
+                launchGameForm.setAttribute("action", "/battle/launch/turn")
+                
+                new_button_1 = launchGameForm.appendChild(document.createElement("button"))
+                new_button_1.setAttribute("type", "submit")
+                new_button_1.setAttribute("name", "chooseMove")
+                new_button_1.setAttribute("value", `${data[0]['moves'][0]}`)
+                new_button_1.textContent = `${data[0]['moves'][0]}`
+    
+                new_button_2 = launchGameForm.appendChild(document.createElement("button"))
+                new_button_2.setAttribute("type", "submit")
+                new_button_2.setAttribute("name", "chooseMove")
+                new_button_2.setAttribute("value", `${data[0]['moves'][1]}`)
+                new_button_2.textContent = `${data[0]['moves'][1]}`
+            })
+    })
 }
 
 function userTurn(){
-
+    launchGameForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        // Create a form with user input.
+        const userFormData = new FormData(this); 
+        // Add the name and value of the pressed button to the form.
+        userFormData.append(e.submitter.name, e.submitter.value); 
+        // Send a fetch request of type POST.
+        const url = '/battle/launch/turn';
+        fetch(url, { 
+            method: 'post', 
+            body: userFormData
+        })
+            .then(response => response.json()) // Read the response as json.
+            .then(data => {
+                // console.log(data)
+                // console.log(data[0]['moves'])
+    
+                new_para = launchGameForm.appendChild(document.createElement("p"))
+    
+                new_para.innerText = `${data[0]['name']} used ${data[0]['move']} causing ${data[0]['damage']} damage.
+                ${data[1]['name']}'s hp was reduced to ${data[1]['hp']}.`
+            });
+    })
 }
 
 
 
 
-launchGameForm.addEventListener('submit', function(e) {
-
-    
-    launchGame.style.display = "none"
-    e.preventDefault();
-
-    // Create a form with user input.
-    const cpuFormData = new FormData(this); 
-    // Add the name and value of the pressed button to the form.
-    cpuFormData.append(e.submitter.name, e.submitter.value); 
-    // Send a fetch request of type POST.
-    const url = '/battle/launch';
-    fetch(url, { 
-        method: 'post', 
-        body: cpuFormData
-    })
-        .then(response => response.json()) // Read the response as json.
-        .then(data => {
-        
-            //console.log(data)
-            //console.log(data[0]['moves'])
-
-            new_para = launchGameForm.appendChild(document.createElement("p"))
-            new_para.innerText = `${data[1]['name']} used ${data[1]['move']} causing ${data[1]['damage']} damage.
-            ${data[0]['name']}'s hp was reduced to ${data[0]['hp']}.
-            Choose your next move!`
-
-            
-            launchGameForm.setAttribute("action", "/battle/launch/turn")
-            
-            new_button_1 = launchGameForm.appendChild(document.createElement("button"))
-            new_button_1.setAttribute("type", "submit")
-            new_button_1.setAttribute("name", "chooseMove")
-            new_button_1.setAttribute("value", `${data[0]['moves'][0]}`)
-            new_button_1.textContent = `${data[0]['moves'][0]}`
-
-            new_button_2 = launchGameForm.appendChild(document.createElement("button"))
-            new_button_2.setAttribute("type", "submit")
-            new_button_2.setAttribute("name", "chooseMove")
-            new_button_2.setAttribute("value", `${data[0]['moves'][1]}`)
-            new_button_2.textContent = `${data[0]['moves'][1]}`
-        })
-    })
 
 
 
-// launchGameForm.addEventListener('submit', function(e) {
-    
-//     launchGame.style.display = "none"
-//     e.preventDefault();
 
-//     // Create a form with user input.
-//     const cpuFormData = new FormData(this); 
-//     // Add the name and value of the pressed button to the form.
-//     cpuFormData.append(e.submitter.name, e.submitter.value); 
-//     // Send a fetch request of type POST.
-//     const url = '/battle/launch';
-//     fetch(url, { 
-//         method: 'post', 
-//         body: cpuFormData
-//     })
-//         .then(response => response.json()) // Read the response as json.
-//         .then(data => {
-//             console.log(data)
-//             console.log(data[0]['moves'])
 
-//             new_para = gamePlay.appendChild(document.createElement("p"))
-//             new_para.innerText = `${data[1]['name']} used ${data[1]['move']} causing ${data[1]['damage']} damage.
-//             ${data[0]['name']}'s hp was reduced to ${data[0]['hp']}.
-//             Choose your next move!`
 
-//             const new_form = document.createElement("form")
 
-//             new_form.setAttribute("method", "POST")
-//             new_form.setAttribute("action", "/battle/launch/turn")
-//             new_form.setAttribute("id", "battleForm")
-//             new_button_1 = new_form.appendChild(document.createElement("button"))
-//             new_button_1.setAttribute("type", "submit")
-//             new_button_1.setAttribute("name", "chooseMove")
-//             new_button_1.setAttribute("value", `${data[0]['moves'][0]}`)
-//             new_button_1.textContent = `${data[0]['moves'][0]}`
 
-//             new_button_2 = new_form.appendChild(document.createElement("button"))
-//             new_button_2.setAttribute("type", "submit")
-//             new_button_2.setAttribute("name", "chooseMove")
-//             new_button_2.setAttribute("value", `${data[0]['moves'][1]}`)
-//             new_button_2.textContent = `${data[0]['moves'][1]}`
 
-//             new_form.addEventListener('submit', function(e) {
-
-//                 e.preventDefault();
-                
-            
-//                 // Create a form with user input.
-//                 const userFormData = new FormData(this); 
-//                 // Add the name and value of the pressed button to the form.
-//                 userFormData.append(e.submitter.name, e.submitter.value); 
-//                 console.log(e.submitter.name)
-//                 console.log(e.submitter.value)
-//                 // Send a fetch request of type POST.
-//                 const url = '/battle/launch/turn';
-//                 fetch(url, { 
-//                     method: 'post', 
-//                     body: userFormData
-//                 })
-//                     .then(response => response.json()) // Read the response as json.
-//                     .then(data => {
-//                         console.log(data)
-//                         console.log(data[0]['moves'])
-            
-//                         new_para = new_form.appendChild(document.createElement("p"))
-            
-//                         new_para.innerText = `${data[0]['name']} used ${data[0]['move']} causing ${data[0]['damage']} damage.
-//                         ${data[1]['name']}'s hp was reduced to ${data[1]['hp']}.`
-//                     });
-//             })
-//             gamePlay.appendChild(new_form)  
-//         });
-// })
 
 getPokemonStats()
+cpuTurn()
+userTurn()
 
 
-// new_form.addEventListener('submit', function(e) {
 
-//     e.preventDefault();
-    
-
-//     // Create a form with user input.
-//     const userFormData = new FormData(this); 
-//     // Add the name and value of the pressed button to the form.
-//     userFormData.append(e.submitter.name, e.submitter.value); 
-//     console.log(e.submitter.name)
-//     console.log(e.submitter.value)
-//     // Send a fetch request of type POST.
-//     const url = '/battle/launch/turn';
-//     fetch(url, { 
-//         method: 'post', 
-//         body: userFormData
-//     })
-//         .then(response => response.json()) // Read the response as json.
-//         .then(data => {
-//             console.log(data)
-//             console.log(data[0]['moves'])
-
-//             new_para = new_form.appendChild(document.createElement("p"))
-
-//             new_para.innerText = `${data[0]['name']} used ${data[0]['move']} causing ${data[0]['damage']} damage.
-//             ${data[1]['name']}'s hp was reduced to ${data[1]['hp']}.`
-//         });
-// })
-// gamePlay.appendChild(new_form)  
-// });
-// })
