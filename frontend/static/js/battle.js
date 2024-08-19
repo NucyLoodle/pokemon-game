bb469d2509cb7f2720cad3b8a916bede114b4a0e
NucyLoodle <lucymeredith3@gmail.com>
2024-08-19 14:47:15 +0100

    write function to get initial moves of pokemon

--- frontend/static/js/battle.js
+++ frontend/static/js/battle.js
@@ -4,26 +4,28 @@ Functions to create the initial battle between player and CPU
 
 // let userPokemonName = "";
 
-function getResponseFromApi(pokemonName) {
-    apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+
+
+function getInitialMoves(pokemonName) {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
 
     fetch(apiUrl)
-        .then(response => {
-        if (!response.ok) {
-        throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+        .then(response => response.json())
+        .then (data => {
+            let dataLength = Object.keys(data).length;
+            let moveNames = []
 
+            for (let i = 0; i < dataLength; i++) {
+                if (data['moves'][i]['version_group_details'][0]['move_learn_method']['name'] == "level-up" && data['moves'][i]['version_group_details'][0]['level_learned_at'] == 1) {
+                    moveNames.push(data['moves'][i]['move']['name'])
+                }        
+            }
+            console.log(moveNames)
+        })   
 }
 
 function getPokemon() {
+    
     const userChoiceForm = document.getElementById('userPokemonChoice')
     userChoiceForm.addEventListener('click', function(e) {
     const isButton = e.target.nodeName === 'BUTTON';
