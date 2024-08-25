bffb2b247662291eb3ccafc60dcee9305cf57009
NucyLoodle <lucymeredith3@gmail.com>
2024-08-25 14:28:29 +0100

    update uservalidation methods)

--- backend/login.py
+++ backend/login.py
@@ -5,51 +5,22 @@ import backend.db_utils as db
 from flask import request
 
 class UserValidation:
-    def __init__(self, first_name, last_name, email, password):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
+    def __init__(self, user_name, password):
+        self.user_name = user_name
         self.password = password
 
     @property
-    def first_name(self):
-        return self.first_name
+    def user_name(self):
+        return self.user_name
 
-    @first_name.setter
-    def first_name(self, value):
+    @user_name.setter
+    def user_name(self, value):
         """
-        Checks for length of first name for UserValidation instance
+        Checks for length of user name for UserValidation instance
         """
-        if len(value) > 20:
-            raise ValueError("First name cannot exceed 20 characters.")
-        self._first_name = value
-
-    @property
-    def last_name(self):
-        return self.last_name
-
-    @last_name.setter
-    def last_name(self, value):
-        """
-        Checks for length of last name for UserValidation instance
-        """
-        if len(value) > 20:
-            raise ValueError("Last name cannot exceed 20 characters.")
-        self._last_name = value
-
-    @property
-    def email(self):
-        return self._email
-
-    @email.setter
-    def email(self, value):
-        """
-        Checks if user has entered a valid email address for UserValidation instance
-        """
-        regex = "[^@]+@[^@]+\.[^@]+"
-        if not re.match(regex, value) or len(value) > 30:
-            raise ValueError("Enter a valid email address")
-        self._email = value
+        if len(value) > 15:
+            raise ValueError("Username cannot exceed 15 characters.")
+        self._user_name = value
 
     @property
     def password(self):
@@ -61,7 +32,7 @@ class UserValidation:
         Checks for length of password for UserValidation instance
         """
         if len(value) >= 20 or len(value) < 4:
-            raise ValueError("Password must be between 4 and 20 characters")
+            raise ValueError("Password must be between 4 and 15 characters")
         self._password = value
 
 
