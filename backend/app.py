931041cb28d09ea2822dafacd6bb2c8746964a26
NucyLoodle <lucymeredith3@gmail.com>
2024-08-25 20:53:07 +0100

    import packages and build login function

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, session
-import flask
 from flask_session import Session
 import battle
+from flask_mysqldb import MySQL
+import MySQLdb.cursors, re, hashlib
+import db_utils as db
+
 
 
 app = Flask(__name__ ,
@@ -10,9 +13,8 @@ app = Flask(__name__ ,
             template_folder='../frontend/templates')
 
 
-# CORS(app)
 app.secret_key = 'BAD_SECRET_KEY'
-
+mysql = MySQL(app)
 
 @app.route("/")
 def main():
@@ -24,59 +26,15 @@ def battle_page():
 
 @app.route("/battle", methods=['POST', 'GET'])
 def get_user_cpu_pokemon():
-    return battle.get_pokemon_data()
+    return battle.get_pokemon()
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    # Here we use a class of some kind to represent and validate our
-    # client-side form data. For example, WTForms is a library that will
-    # handle this for us, and we use a custom LoginForm to validate.
-    form = LoginForm()
-    if form.validate_on_submit():
-        # Login and validate the user.
-        # user should be an instance of your `User` class
-        login_user(user)
+@app.route("/battle/launch", methods=['GET', 'POST'])
+def cpu_move():
+    return battle.cpu_turn()
 
-        flask.flash('Logged in successfully.')
-
-        next = flask.request.args.get('next')
-        # url_has_allowed_host_and_scheme should check if the url is safe
-        # for redirects, meaning it matches the request host.
-        # See Django's url_has_allowed_host_and_scheme for an example.
-
-        def url_has_allowed_host_and_scheme(url, allowed_hosts, require_https=False):
-            """
-            Return ``True`` if the url uses an allowed host and a safe scheme.
-
-            Always return ``False`` on an empty url.
-
-            If ``require_https`` is ``True``, only 'https' will be considered a valid
-            scheme, as opposed to 'http' and 'https' with the default, ``False``.
-
-            Note: "True" doesn't entail that a URL is "safe". It may still be e.g.
-            quoted incorrectly. Ensure to also use django.utils.encoding.iri_to_uri()
-            on the path component of untrusted URLs.
-            """
-            if url is not None:
-                url = url.strip()
-            if not url:
-                return False
-            if allowed_hosts is None:
-                allowed_hosts = set()
-            elif isinstance(allowed_hosts, str):
-                allowed_hosts = {allowed_hosts}
-            # Chrome treats \ completely as / in paths but it could be part of some
-            # basic auth credentials so we need to check both URLs.
-            return (
-                url_has_allowed_host_and_scheme(url, allowed_hosts, require_https=require_https) and
-                url_has_allowed_host_and_scheme(url.replace('\\', '/'), allowed_hosts, require_https=require_https)
-            )
-
-        if not url_has_allowed_host_and_scheme(next, request.host):
-            return flask.abort(400)
-
-        return flask.redirect(next or flask.url_for('index'))
-    return flask.render_template('login.html', form=form)
+@app.route("/battle/launch/turn", methods=['GET', 'POST'])
+def user_move():
+    return battle.user_turn()
 
 
 if __name__ == '__main__':
