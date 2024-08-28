d1cc7e857c7a702663cb2bb128ff904b048a627b
NucyLoodle <lucymeredith3@gmail.com>
2024-08-28 10:20:00 +0100

    display stats to user

--- frontend/static/js/profile.js
+++ frontend/static/js/profile.js
@@ -21,21 +21,48 @@ viewParty.addEventListener('submit', function(e) {
             console.log(data[0])
 
             let numOfPokemon = data.length;
-            for (let i = 0; i < numOfPokemon; i++) { 
+            for (let i = 0; i < numOfPokemon; i++) {
+ 
                 div = document.createElement("div")
                 div.setAttribute("class","partyPokemonStats")
                 usersPartyPokemon.append(div)
+
                 namePara = document.createElement("p")
                 div.append(namePara)
                 namePara.innerText = data[i]['pokemon_name'].toUpperCase()
-                }
+                
+                typePara = document.createElement("p")
+                div.append(typePara)
+                typePara.innerText = "Type: " + data[i]['pokemon_type']
+
+                levelPara = document.createElement("p")
+                div.append(levelPara)
+                levelPara.innerText = "Level: " + data[i]['level']
+
+                expPara = document.createElement("p")
+                div.append(expPara)
+                expPara.innerText = "XP: " + data[i]['exp']
+
+                hpPara = document.createElement("p")
+                div.append(hpPara)
+                hpPara.innerText = "HP: " + data[i]['hp']
+
+
+            }
             viewPartyButton.style.display = "none"
-            
-
-
         })
     })   
 
 
 
 
+    // (2) [{…}, {…}]
+    // 0
+    // : 
+    // {exp: 64, hp: 45, level: 1, pokemon_name: 'bulbasaur', pokemon_type: 'grass', …}
+    // 1
+    // : 
+    // {exp: 62, hp: 39, level: 1, pokemon_name: 'charmander', pokemon_type: 'fire', …}
+    // length
+    // : 
+    // 2
