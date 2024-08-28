2a4f3f706c877dec63c9e9a9779e9893522b5ae0
NucyLoodle <lucymeredith3@gmail.com>
2024-08-28 16:35:46 +0100

    remove old print statements

--- backend/add_pokemon.py
+++ backend/add_pokemon.py
@@ -1,6 +1,4 @@
-from flask import request, session
 import db_utils as db
-import battle 
 
 def pokemon_already_caught(user_id, pokemon_id):
     query = """
@@ -13,26 +11,19 @@ def pokemon_already_caught(user_id, pokemon_id):
         return False
 
 def add_new_pokemon(user_id, pokemon_id, user_pokemon_name):
-    
     query = """
             INSERT INTO pokemon (user_id, pokemon_id, pokemon_name)
             VALUES (%s, %s, %s);
             """
     db.connect_db(query, (user_id, pokemon_id, user_pokemon_name,)) 
-    print("yay")
     return f"success {user_pokemon_name} added to your party"
    
-
-
 def add_pokemon_stats(user_id, user_pokemon_name, pokemon_type, hp, level, exp, weight):
-    #user_id = session['id']
     query = """
-    INSERT INTO pokemon_stats(pokemon_db_id, pokemon_type, hp, level, exp, weight)
-    VALUES ((SELECT pokemon_db_id FROM pokemon p WHERE p.user_id = %s AND p.pokemon_name = %s), %s, %s, %s, %s, %s); 
-
-    """ # problem with multiple pokemon
+            INSERT INTO pokemon_stats(pokemon_db_id, pokemon_type, hp, level, exp, weight)
+            VALUES ((SELECT pokemon_db_id FROM pokemon p WHERE p.user_id = %s AND p.pokemon_name = %s), %s, %s, %s, %s, %s); 
+            """ 
     db.connect_db(query, (user_id, user_pokemon_name, pokemon_type, hp, level, exp, weight,)) # edit type is now in new table
-    print("yay")
     return f"success"
 
 def add_pokemon_sprites(user_id, user_pokemon_name, pokemon_sprite):
