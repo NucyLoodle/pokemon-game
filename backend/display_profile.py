ad88a23aee4e69021af62a4df656fafe43cd6652
NucyLoodle <lucymeredith3@gmail.com>
2024-08-28 08:44:25 +0100

    pull pokemon db id to use in next functions

--- backend/display_profile.py
+++ backend/display_profile.py
@@ -11,8 +11,12 @@ def get_pokemon_names(user_id):
     # access the db to get a list of user's pokemon
 
     query = """
-            SELECT pokemon_name FROM pokemon p WHERE p.user_id = %s
+            SELECT pokemon_name, pokemon_db_id FROM pokemon p WHERE p.user_id = %s
             """
     result = db.connect_db_multiple_results(query, (user_id,))
     return result
 
+
+
+
+# def get_pokemon_stats
