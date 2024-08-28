7291c15a8fde14e572b2a456f4180c1e4f6ab233
NucyLoodle <lucymeredith3@gmail.com>
2024-08-28 16:34:24 +0100

    remove unused code and import statements

--- backend/display_profile.py
+++ backend/display_profile.py
@@ -1,11 +1,4 @@
-from flask import session, jsonify
 import db_utils as db
-# display images of user's pokemon
-# display name
-# display stats
-
-
-
 
 def get_pokemon_info(user_id):
     
@@ -13,10 +6,10 @@ def get_pokemon_info(user_id):
 
     query = """
             SELECT pokemon.pokemon_name, pokemon_stats.pokemon_type, pokemon_stats.hp, pokemon_stats.level, pokemon_stats.exp, pokemon_stats.weight, pokemon_sprites.pokemon_sprite
-FROM pokemon
-INNER JOIN pokemon_stats ON pokemon.pokemon_db_id = pokemon_stats.pokemon_db_id
-INNER JOIN pokemon_sprites ON pokemon_stats.pokemon_db_id = pokemon_sprites.pokemon_db_id
-WHERE pokemon_stats.pokemon_db_id IN (SELECT pokemon.pokemon_db_id FROM pokemon WHERE pokemon.user_id = %s);
+            FROM pokemon
+            INNER JOIN pokemon_stats ON pokemon.pokemon_db_id = pokemon_stats.pokemon_db_id
+            INNER JOIN pokemon_sprites ON pokemon_stats.pokemon_db_id = pokemon_sprites.pokemon_db_id
+            WHERE pokemon_stats.pokemon_db_id IN (SELECT pokemon.pokemon_db_id FROM pokemon WHERE pokemon.user_id = %s);
             """
     result = db.connect_db_multiple_results(query, (user_id,))
     return result
