86f4885be4544c635df243af13824e374305cfbd
NucyLoodle <lucymeredith3@gmail.com>
2024-08-29 21:04:28 +0100

    write query to get pokemon info from db

---
+++ backend/main_battle.py
@@ -0,0 +1,15 @@
+import db_utils as db
+
+def get_pokemon_info_for_battle(user_id):
+    
+    # access the db to get a list of user's pokemon and their stats
+
+    query = """
+            SELECT pokemon.pokemon_name, pokemon_moves.move_1, pokemon_moves.move_2, pokemon_moves.move_3, pokemon_moves.move_4, pokemon_sprites.pokemon_sprite
+            FROM pokemon
+            INNER JOIN pokemon_moves ON pokemon.pokemon_db_id = pokemon_moves.pokemon_db_id
+            INNER JOIN pokemon_sprites ON pokemon_moves.pokemon_db_id = pokemon_sprites.pokemon_db_id
+            WHERE pokemon_moves.pokemon_db_id IN (SELECT pokemon.pokemon_db_id FROM pokemon WHERE pokemon.user_id = %s);
+            """
+    result = db.connect_db_multiple_results(query, (user_id,))
+    return result
