874eda53eab3fe28298f2694be0f73260116eac8
NucyLoodle <lucymeredith3@gmail.com>
2024-08-29 21:49:50 +0100

    display stats and moves

--- frontend/static/js/battle.js
+++ frontend/static/js/battle.js
@@ -1,15 +1,22 @@
+function handleMovesArray(a) {
+    a = a.map(function(x){ return x.toUpperCase(); })
+    return a.length == 1 ? a[0] : [ a.slice(0, a.length - 1).join(", "), a[a.length - 1] ].join(" and ")
+  }
+
 const userFlag = 
     fetch('/battle/party')
         .then(r=>r.json())
         .then(data => {
     // return data;
+    console.log(data)
     
     let numOfPokemon = data.length;
         for (let i = 0; i < numOfPokemon; i++) {
             div = document.createElement("div")
             div.setAttribute("class","partyPokemonStats")
             usersPartyPokemon.append(div)
-
+            
+            // change name to be a button
             namePara = document.createElement("p")
             div.append(namePara)
             namePara.innerText = data[i]['pokemon_name'].toUpperCase()
@@ -34,6 +41,29 @@ const userFlag =
             hpPara = document.createElement("p")
             div.append(hpPara)
             hpPara.innerText = "HP: " + data[i]['hp']
+            
+
+            const moves_list = []
+            if (data[i]['move_1'] != "null") {
+                moves_list.push(data[i]['move_1'])
+            }
+            if (data[i]['move_2'] != "null") {
+                moves_list.push(data[i]['move_2'])
+            }
+            if (data[i]['move_3'] != "null") {
+                moves_list.push(data[i]['move_3'])
+            }
+            if (data[i]['move_4'] != "null") {
+                moves_list.push(data[i]['move_4'])
+            }
+
+
+
+            movesPara = document.createElement("p")
+            div.append(movesPara)
+            movesPara.innerText = "Moves: " + `${handleMovesArray(moves_list)}`
+            
+            
         }
 
 });
