0d9d43c628ce7096c7b01987094bda34c2fb1387
NucyLoodle <lucymeredith3@gmail.com>
2024-08-29 20:53:34 +0100

    display party pokemon

--- frontend/static/js/battle.js
+++ frontend/static/js/battle.js
@@ -2,9 +2,41 @@ const userFlag =
     fetch('/battle/party')
         .then(r=>r.json())
         .then(data => {
-    return data;
+    // return data;
+    
+    let numOfPokemon = data.length;
+        for (let i = 0; i < numOfPokemon; i++) {
+            div = document.createElement("div")
+            div.setAttribute("class","partyPokemonStats")
+            usersPartyPokemon.append(div)
+
+            namePara = document.createElement("p")
+            div.append(namePara)
+            namePara.innerText = data[i]['pokemon_name'].toUpperCase()
+
+            spriteImg = document.createElement("img")
+            spriteImg.setAttribute("src", `${data[i]["pokemon_sprite"]}`)
+            div.append(spriteImg)
+
+            
+            typePara = document.createElement("p")
+            div.append(typePara)
+            typePara.innerText = "Type: " + data[i]['pokemon_type']
+
+            levelPara = document.createElement("p")
+            div.append(levelPara)
+            levelPara.innerText = "Level: " + data[i]['level']
+
+            expPara = document.createElement("p")
+            div.append(expPara)
+            expPara.innerText = "XP: " + data[i]['exp']
+
+            hpPara = document.createElement("p")
+            div.append(hpPara)
+            hpPara.innerText = "HP: " + data[i]['hp']
+        }
+
 });
 window.onload = async () => {
     let someData = await userFlag;
-    console.log(someData)
 };
