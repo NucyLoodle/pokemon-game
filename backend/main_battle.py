a2d39dee92bf0ab169b10a7129cd0f17f0dd5de1
NucyLoodle <lucymeredith3@gmail.com>
2024-08-29 21:49:30 +0100

    edit query to get stats

--- backend/main_battle.py
+++ backend/main_battle.py
@@ -5,10 +5,11 @@ def get_pokemon_info_for_battle(user_id):
     # access the db to get a list of user's pokemon and their stats
 
     query = """
-            SELECT pokemon.pokemon_name, pokemon_moves.move_1, pokemon_moves.move_2, pokemon_moves.move_3, pokemon_moves.move_4, pokemon_sprites.pokemon_sprite
+            SELECT pokemon.pokemon_name, pokemon_moves.move_1, pokemon_moves.move_2, pokemon_moves.move_3, pokemon_moves.move_4, pokemon_sprites.pokemon_sprite, pokemon_stats.pokemon_type, pokemon_stats.hp, pokemon_stats.level, pokemon_stats.exp, pokemon_stats.weight
             FROM pokemon
             INNER JOIN pokemon_moves ON pokemon.pokemon_db_id = pokemon_moves.pokemon_db_id
             INNER JOIN pokemon_sprites ON pokemon_moves.pokemon_db_id = pokemon_sprites.pokemon_db_id
+            INNER JOIN pokemon_stats ON pokemon_sprites.pokemon_db_id = pokemon_stats.pokemon_db_id
             WHERE pokemon_moves.pokemon_db_id IN (SELECT pokemon.pokemon_db_id FROM pokemon WHERE pokemon.user_id = %s);
             """
     result = db.connect_db_multiple_results(query, (user_id,))
