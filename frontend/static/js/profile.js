ccf93778af8e870e4bdf5a2799472f226822046f
NucyLoodle <lucymeredith3@gmail.com>
2024-09-02 14:56:15 +0100

    change button to paragraph if only 1 pokemon left

--- frontend/static/js/profile.js
+++ frontend/static/js/profile.js
@@ -64,60 +64,66 @@ viewParty.addEventListener('submit', function(e) {
                 div.setAttribute("class","partyPokemonStats")
                 usersPartyPokemon.append(div)
 
-                nameButton = document.createElement("button")
-                nameButton.setAttribute("value", `${data[i]['pokemon_name']}`)
-                //nameButton.setAttribute("onclick", "showPopup()")
-                nameButton.setAttribute("class", "popup")
-                nameButton.innerText = data[i]['pokemon_name'].toUpperCase()
+                if (numOfPokemon > 1) {
+                    nameButton = document.createElement("button")
+                    nameButton.setAttribute("value", `${data[i]['pokemon_name']}`)
+                    nameButton.setAttribute("class", "popup")
+                    nameButton.innerText = data[i]['pokemon_name'].toUpperCase()
 
 
-                releaseSpan = document.createElement("span")
-                releaseSpan.setAttribute("class", "popuptext")
-                releaseSpan.setAttribute("id", `${data[i]['pokemon_name']}`)
-                releaseSpan.style.display = "none"
+                    releaseSpan = document.createElement("span")
+                    releaseSpan.setAttribute("class", "popuptext")
+                    releaseSpan.setAttribute("id", `${data[i]['pokemon_name']}`)
+                    releaseSpan.style.display = "none"
 
-                releaseForm = document.createElement("form")
-                releaseForm.setAttribute('id', 'releaseForm')
-                releaseForm.setAttribute('method', 'DELETE') // should this be delete
-                releaseForm.setAttribute('action', '/profile/release')
-                releaseSpan.append(releaseForm)
+                    releaseForm = document.createElement("form")
+                    releaseForm.setAttribute('id', 'releaseForm')
+                    releaseForm.setAttribute('method', 'DELETE') // should this be delete
+                    releaseForm.setAttribute('action', '/profile/release')
+                    releaseSpan.append(releaseForm)
 
-                releaseButton = document.createElement("button")
-                releaseButton.innerText = `Release ${data[i]['pokemon_name'].toUpperCase()}?`
-                releaseButton.setAttribute('name', 'releaseButton')
-                releaseButton.setAttribute('value', `${data[i]['pokemon_name']}`)
-                //releaseButton.setAttribute('onclick', )
+                    releaseButton = document.createElement("button")
+                    releaseButton.innerText = `Release ${data[i]['pokemon_name'].toUpperCase()}?`
+                    releaseButton.setAttribute('name', 'releaseButton')
+                    releaseButton.setAttribute('value', `${data[i]['pokemon_name']}`)
+                    //releaseButton.setAttribute('onclick', )
 
-                nameButton.addEventListener("click", function(){
-                    pokemonSpan = document.getElementById(`${data[i]['pokemon_name']}`)
+                    nameButton.addEventListener("click", function(){
+                        pokemonSpan = document.getElementById(`${data[i]['pokemon_name']}`)
 
-                    if (pokemonSpan.style.display == "none") {
-                        pokemonSpan.style.display = "flex"
-                    } else {
-                        pokemonSpan.style.display = "none"
-                    }
-                })
+                        if (pokemonSpan.style.display == "none") {
+                            pokemonSpan.style.display = "flex"
+                        } else {
+                            pokemonSpan.style.display = "none"
+                        }
+                    })
 
-                releaseForm.addEventListener("submit", function(e) { 
-                    
-                    e.preventDefault()
-                              
-                    const formData = new FormData(this); 
-                    formData.append(e.submitter.name, e.submitter.value);
-                    const url = '/profile/release';
-                        fetch(url, { 
-                            method: 'DELETE', 
-                            body: formData // send user choice through to python backend
-                        })
-                            .then(response => response.text()) 
-                            .then(data => {
-                                on()
-                                releaseText.innerText = data
+                    releaseForm.addEventListener("submit", function(e) { 
+                        
+                        e.preventDefault()
+                                
+                        const formData = new FormData(this); 
+                        formData.append(e.submitter.name, e.submitter.value);
+                        const url = '/profile/release';
+                            fetch(url, { 
+                                method: 'DELETE', 
+                                body: formData // send user choice through to python backend
                             })
-                        })
-                div.append(nameButton)
-                nameButton.append(releaseSpan)
-                releaseForm.append(releaseButton)
+                                .then(response => response.text()) 
+                                .then(data => {
+                                    on()
+                                    releaseText.innerText = data
+                                })
+                            })
+                    div.append(nameButton)
+                    nameButton.append(releaseSpan)
+                    releaseForm.append(releaseButton)
+                } else {
+                    namePara = document.createElement('p')
+                    namePara.innerText = data[i]['pokemon_name'].toUpperCase()
+                    div.append(namePara)
+                }
+                
 
 
 
