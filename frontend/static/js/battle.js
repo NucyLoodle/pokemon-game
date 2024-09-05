9873002b1b6afe17396052e9030759050772a454
NucyLoodle <lucymeredith3@gmail.com>
2024-09-05 20:47:24 +0100

    edit wording when damage is 0

--- frontend/static/js/battle.js
+++ frontend/static/js/battle.js
@@ -198,9 +198,13 @@ function cpuTurn(cpuPokemonName, cpuPokemonMoves, userPokemonHp, userPokemonName
         userPokemonHp = sessionStorage.getItem("userPokemonHp") - cpuDamage
         sessionStorage.setItem("userPokemonHp", userPokemonHp)
         if (userPokemonHp > 0) {
-            createButtonsForUser(userPokemonMoves)
-            return `${cpuPokemonName.toUpperCase()} used ${cpuMove.toUpperCase()} causing ${cpuDamage} damage!
-            ${userPokemonName.toUpperCase()}'s hp was reduced to ${userPokemonHp}!`
+            if (cpuDamage == 0) {
+                return `${cpuPokemonName.toUpperCase()} used ${cpuMove.toUpperCase()}. It had no effect!`
+              } else {
+                createButtonsForUser(userPokemonMoves)
+                return `${cpuPokemonName.toUpperCase()} used ${cpuMove.toUpperCase()} causing ${cpuDamage} damage!
+                ${userPokemonName.toUpperCase()}'s hp was reduced to ${userPokemonHp}!`
+              }
         } else {
             document.querySelectorAll('.oldPara').forEach(para => para.style.display = "none")
             document.querySelectorAll('button').forEach(button => button.style.display = "none") //hide user choice buttons after selection
@@ -219,8 +223,12 @@ if (userPokemonHp > 0 && cpuPokemonHp > 0) {
     cpuPokemonHp = sessionStorage.getItem("cpuPokemonHp") - userDamage
     sessionStorage.setItem("cpuPokemonHp", cpuPokemonHp)
     if (cpuPokemonHp > 0) {
-        return `${userPokemonName.toUpperCase()} used ${userMove.toUpperCase()} causing ${userDamage} damage!
-        ${cpuPokemonName.toUpperCase()}'s hp was reduced to ${cpuPokemonHp}.`
+        if (userDamage == 0) {
+            return `${userPokemonName.toUpperCase()} used ${userMove.toUpperCase()}. It had no effect!`
+          } else {
+            return `${userPokemonName.toUpperCase()} used ${userMove.toUpperCase()} causing ${userDamage} damage!
+          ${cpuPokemonName.toUpperCase()}'s hp was reduced to ${cpuPokemonHp}.`
+          }
     } else {
         document.querySelectorAll('.oldPara').forEach(para => para.style.display = "none")
         document.querySelectorAll('button').forEach(button => button.style.display = "none") //hide user choice buttons after selection
